dd533f696f68e9c9a52ea58c1d770cf5488cf0d1
Potatoyz908 <euller2005@gmail.com>
2025-02-28 17:55:54 -0300

    feat(Docker): Atualiza Dockerfile e entrypoint.sh para coletar estáticos e formatar código

--- API/entrypoint.sh
+++ API/entrypoint.sh
@@ -3,17 +3,17 @@
 set -e
 
 echo "Executando Black para formatar o código..."
-black .  # Executa o Black para formatar o código
+black .  
 
 echo "Executando Ruff para verificar a formatação do código..."
 ruff . --fix
 
 if [ "$1" = "python" ] && [ "$2" = "manage.py" ]; then
   echo "Gerando novas migrações..."
-  python manage.py makemigrations  # Cria novas migrações com base nos modelos
+  python manage.py makemigrations 
 
   echo "Iniciando migrações do banco de dados..."
-  python manage.py migrate  # Executa o migrate automaticamente
+  python manage.py migrate 
 
   echo "Migrações concluídas."
 fi
